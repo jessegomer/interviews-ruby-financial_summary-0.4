ca2f97fdba53fff16bf5af2edcefbcfe4b9aceff
Jesse Gomer <jessegomer@gmail.com>
2018-06-18 00:38:06 -0700

    Add more specs and finished summary

--- app/models/financial_summary.rb
+++ app/models/financial_summary.rb
@@ -1,32 +1,50 @@
 class FinancialSummary
-  attr_reader :user
+  attr_reader :user, :currency, :start_date
 
-  def self.one_day(user=nil, currency=:usd)
-    new(user).generate_summary(currency, Time.now - 1.days)
+  def self.one_day(user:, currency: :usd)
+    new(user: user, currency: currency, start_date: Time.now - 1.days)
   end
 
-  def self.seven_days(user=nil, currency=:usd)
-    new(user).generate_summary(currency, Time.now - 7.days)
+  def self.seven_days(user:, currency: :usd)
+    new(user: user, currency: currency, start_date: Time.now - 7.days)
   end
 
-  def self.lifetime(user=nil, currency=:usd)
-    new.generate_summary(user, currency)
+  def self.lifetime(user:, currency: :usd)
+    new(user: user, currency: currency)
   end
 
-  def initialize(user)
-    if user.nil?
-      raise ArgumentError('FinancialSummary requires a user')
-    end
+  def initialize(user:, currency: :usd, start_date: nil)
     @user = user
+    @currency = currency.to_s.upcase
+    @start_date = start_date
+
+    @counts = Hash.new(0)
+    @amounts = Hash.new{|hash, key| hash[key] = Money.from_amount(0, currency)}
+
+    load_summary
   end
 
-  def generate_summary(currency=:usd, start_date=nil)
-    transactions = user.trac
+  def load_summary
+    transactions = Transaction.where(user_id: user.id, amount_currency: currency)
 
+    if start_date.present?
+      transactions = transactions.where('created_at >= ?', start_date )
+    end
 
+    transactions.each do |transaction|
+      category = transaction.category.to_sym
+      @counts[category] += 1
+      @amounts[category] += transaction.amount
+    end
 
   end
 
+  def amount(category)
+    @amounts[category]
+  end
 
+  def count(category)
+    @counts[category]
+  end
 
 end
--- spec/models/financial_summary_spec.rb
+++ spec/models/financial_summary_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe FinancialSummary do
   it 'summarizes over one day' do
-    pending('Not implemented yet')
 
     user = create(:user)
 
@@ -21,7 +20,6 @@ describe FinancialSummary do
   end
 
   it 'summarizes over seven days' do
-    pending('Not implemented yet')
 
     user = create(:user)
 
@@ -40,7 +38,6 @@ describe FinancialSummary do
   end
 
   it 'summarizes over lifetime' do
-    pending('Not implemented yet')
 
     user = create(:user)
 
@@ -57,4 +54,65 @@ describe FinancialSummary do
     expect(subject.count(:deposit)).to eq(3)
     expect(subject.amount(:deposit)).to eq(Money.from_amount(13.12, :usd))
   end
+
+  it 'selects the correct currency' do
+    user = create(:user)
+
+    create(:transaction, user: user, category: :deposit, amount: Money.from_amount(2.12, :usd))
+    create(:transaction, user: user, category: :deposit, amount: Money.from_amount(10, :usd))
+
+    create(:transaction, user: user, category: :deposit, amount: Money.from_amount(40.21, :cad))
+    create(:transaction, user: user, category: :deposit, amount: Money.from_amount(33, :cad))
+
+    subject = FinancialSummary.lifetime(user: user, currency: :cad)
+    expect(subject.count(:deposit)).to eq(2)
+    expect(subject.amount(:deposit)).to eq(Money.from_amount(73.21, :cad))
+  end
+
+  it 'handles multiple types of transactions' do
+    user = create(:user)
+
+    create(:transaction, user: user, category: :deposit, amount: Money.from_amount(2.12, :usd))
+    create(:transaction, user: user, category: :deposit, amount: Money.from_amount(10, :usd))
+
+    create(:transaction, user: user, category: :refund, amount: Money.from_amount(40.21, :usd))
+
+    create(:transaction, user: user, category: :withdraw, amount: Money.from_amount(10, :usd))
+    create(:transaction, user: user, category: :withdraw, amount: Money.from_amount(20, :usd))
+    create(:transaction, user: user, category: :withdraw, amount: Money.from_amount(30, :usd))
+
+    subject = FinancialSummary.lifetime(user: user, currency: :usd)
+
+    expect(subject.count(:deposit)).to eq(2)
+    expect(subject.amount(:deposit)).to eq(Money.from_amount(12.12, :usd))
+    expect(subject.count(:refund)).to eq(1)
+    expect(subject.amount(:refund)).to eq(Money.from_amount(40.21, :usd))
+    expect(subject.count(:withdraw)).to eq(3)
+    expect(subject.amount(:withdraw)).to eq(Money.from_amount(60, :usd))
+  end
+
+  it 'handles empty summaries' do
+    user = create(:user)
+    subject = FinancialSummary.lifetime(user: user, currency: :usd)
+
+    expect(subject.count(:deposit)).to eq(0)
+    expect(subject.amount(:deposit)).to eq(Money.from_amount(0, :usd))
+  end
+
+  it 'summarizes when there are multiple users' do
+    user_1 = create(:user)
+    user_2 = create(:user)
+    
+    create(:transaction, user: user_1, category: :deposit, amount: Money.from_amount(15, :usd))
+    create(:transaction, user: user_2, category: :deposit, amount: Money.from_amount(10, :usd))
+    create(:transaction, user: user_2, category: :deposit, amount: Money.from_amount(10, :usd))
+
+    user_1_summary = FinancialSummary.lifetime(user: user_1, currency: :usd)
+    user_2_summary = FinancialSummary.lifetime(user: user_2, currency: :usd)
+
+    expect(user_1_summary.count(:deposit)).to eq(1)
+    expect(user_2_summary.count(:deposit)).to eq(2)
+  end
+
+
 end
